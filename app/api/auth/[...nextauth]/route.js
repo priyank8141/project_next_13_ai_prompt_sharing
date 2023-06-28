4ad824771cfd2b5ddc4e58f06610e7039b591a56
priyank8141 <jainpriyank789@gmail.com>
2023-06-28 18:24:35 +0530

    fix: secret error debug

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,6 +4,8 @@ import GoogleProvider from 'next-auth/providers/google';
 import User from '@models/user';
 import { connectToDB } from '@utils/database';
 
+console.log(process.env.GOOGLE_ID)
+console.log(process.env.GOOGLE_CLIENT_SECRET)
 const handler = NextAuth({
   providers: [
     GoogleProvider({
