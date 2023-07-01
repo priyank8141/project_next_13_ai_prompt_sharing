4d849171a1c2b5df0d34a1f0845b56b088e181b1
priyank8141 <jainpriyank789@gmail.com>
2023-07-01 22:54:13 +0530

    fix: cacning

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -7,7 +7,9 @@ export const GET = async (request) => {
 
         const prompts = await Prompt.find({}).sort({ likes: -1 }).populate('creator')
 
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        const response = new Response(JSON.stringify(prompts), { status: 200 })
+        response.headers.set('Cache-Control', 'no-store');
+        return response
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
     }
