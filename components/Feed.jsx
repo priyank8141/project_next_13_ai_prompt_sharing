4a37c7ebb72f5287361723b220f38a8cf3679a24
priyank8141 <jainpriyank789@gmail.com>
2023-07-08 22:53:07 +0530

    fix:cache issue 1

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -28,9 +28,7 @@ const Feed = () => {
   const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
-    const response = await fetch(`/api/prompt`, {
-      revalidate: 0,
-    });
+    const response = await fetch(`/api/prompt`, { next: { revalidate: 10 } });
     const data = await response.json();
 
     setAllPosts(data.prompts);
